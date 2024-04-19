2c6f4ab1e10c6c807feb7505e3fdad60a68ff7af
HarshaSara <148731010+HarshaSara@users.noreply.github.com>
2024-04-19 11:32:17 -0400

    Update Provider.tf

--- Provider.tf
+++ Provider.tf
@@ -9,7 +9,7 @@ terraform {
 provider "azurerm" {
     subscription_id = "c6ed2e43-14ce-4ba0-956c-2a0f9a37dcc4"
     tenant_id = "a4987b6a-2f79-41b5-8b37-5d244725b8ae"
-    client_id = "0450008b-a21b-404d-852d-54647c242607"
+    client_id = "4fed72b8-57ef-4f54-aa98-6032c532a276"
     features {}
   
 }
